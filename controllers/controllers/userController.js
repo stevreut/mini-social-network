33df1f3657bff650434e1e481978bbcca5e6e796
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-17 15:34:42 -0400

    suppress __v from main user get

--- controllers/controllers/userController.js
+++ controllers/controllers/userController.js
@@ -7,7 +7,8 @@ const User = require('../../models/User');  // TODO - altered from original
 module.exports = {
   async getUsers(req, res) {
     try {
-      const users = await User.find();
+      const users = await User.find()
+        .select('-__v');
       res.json(users);
     } catch (err) {
       res.status(500).json(err);
@@ -18,7 +19,7 @@ module.exports = {
     try {
       const user = await User.findOne({ _id: req.params.userId })
         .select('-__v')
-        .populate('friends');  // TODO - added by SR 9/17 12:24 p.m.
+        .populate('friends').select('-__v');  // TODO - added by SR 9/17 12:24 p.m.
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
